4c7eb01a0c660463d622a901f017b3a021fbf1c5
Przemyslaw Ozgo <linux@ozgo.info>
2018-02-12 20:12:34 +0000

    Added Support for git fetch repository with docs

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,23 @@
 FROM alpine:latest
 
-ENV MKDOCS_VERSION="0.17.2"
+ENV MKDOCS_VERSION="0.17.2" \
+    GIT_REPO='false' 
 
 RUN \
     apk add --update \
         ca-certificates \
         bash \
+        git \
+        openssh \
         python2 \
         python2-dev \
-        py-setuptools && \
-    easy_install-2.7 pip && \
-    pip install mkdocs==${MKDOCS_VERSION} && \
+        py-setuptools; \
+    easy_install-2.7 pip; \
+    pip install mkdocs==${MKDOCS_VERSION}; \
     rm -rf /tmp/* /var/tmp/* /var/cache/apk/* /var/cache/distfiles/*
 
 COPY container-files /
+
+WORKDIR /workdir
 
 ENTRYPOINT ["/bootstrap.sh"]
--- container-files/bootstrap.sh
+++ container-files/bootstrap.sh
@@ -2,14 +2,27 @@
 set -eu
 #### "Magic starts Here" - H. Potter #####
 check_install_status () {
+    cd /workdir/mkdocs
     if [[ ! -e "mkdocs.yml" ]]; then
     echo "No previous config. Starting fresh instalation"
     mkdocs new .
     fi
 }
 start_mkdocs () {
+    cd /workdir/mkdocs
     echo "Starting MKDocs"
     mkdocs serve -a $(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1):8000
 }
-check_install_status
-start_mkdocs
+
+get_docs () {
+    echo "Downloading documentation from Git Repository"
+    git clone ${GIT_REPO} /workdir/mkdocs
+}
+
+if [ ${GIT_REPO} != 'false' ]; then
+    get_docs
+    start_mkdocs
+else
+    check_install_status
+    start_mkdocs
+fi
