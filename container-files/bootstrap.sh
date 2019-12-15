9b00877c3d1ca2b7b91ef832af6b3266b5269cb5
Przemek Ozgo <linux@ozgo.info>
2019-12-15 17:46:35 +0000

    Issue-#8: Fixed autoreload (#12)

--- container-files/bootstrap.sh
+++ container-files/bootstrap.sh
@@ -2,37 +2,40 @@
 set -eu
 #### "Magic starts Here" - H. Potter #####
 check_install_status () {
-    if [[ ! -e /workdir/mkdocs ]]; then
-        mkdir -p /workdir/mkdocs
-    fi
-    cd /workdir/mkdocs
-    if [[ ! -e "mkdocs.yml" ]]; then
+  if [[ ! -e /workdir/mkdocs ]]; then
+    echo "No documentation folder available. Creating new one."
+    mkdir -p /workdir/mkdocs
+  fi
+  cd /workdir/mkdocs
+  if [[ ! -e "mkdocs.yml" ]]; then
     echo "No previous config. Starting fresh instalation"
     mkdocs new .
-    fi
+  fi
 }
 start_mkdocs () {
-    if [[ ${LIVE_RELOAD_SUPPORT} == 'true' ]]; then
-        LRS='--no-livereload'
-    else
-        LRS=''
-    fi
-    cd /workdir/mkdocs
-    echo "Starting MKDocs"
-    mkdocs serve -a 0.0.0.0:8000 $LRS
+  if [[ ${LIVE_RELOAD_SUPPORT} != 'true' ]]; then
+    echo "[ DISABLED ] - livereload"
+    LRS='--no-livereload'
+  else
+    echo "[ ENABLED ] - livereload"
+    LRS=''
+  fi
+  cd /workdir/mkdocs
+  echo "Starting MKDocs"
+  mkdocs serve -a 0.0.0.0:8000 $LRS
 }
 
 get_docs () {
-    if [[ ! -e /workdir/mkdocs ]]; then
-        echo "Downloading documentation from Git Repository"
-        git clone ${GIT_REPO} /workdir/mkdocs
-    fi
+  if [[ ! -e /workdir/mkdocs ]]; then
+    echo "Downloading documentation from Git Repository"
+    git clone ${GIT_REPO} /workdir/mkdocs
+  fi
 }
 
 if [ ${GIT_REPO} != 'false' ]; then
-    get_docs
-    start_mkdocs
+  get_docs
+  start_mkdocs
 else
-    check_install_status
-    start_mkdocs
+  check_install_status
+  start_mkdocs
 fi
